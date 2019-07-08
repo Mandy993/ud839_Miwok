bf4d4b4aa699bea0a5bc9d4f1e8279eb74a54c09
shimanyuan <yuanshiman1993@gmail.com>
2019-07-08 22:26:15 +0800

    delete two lines in coloractivity

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,8 +15,7 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
 
 public class ColorsActivity extends AppCompatActivity {
 
